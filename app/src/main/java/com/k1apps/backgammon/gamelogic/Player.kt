12cf731dcb4e5442c73516f71fab8b3aa3f77cf7
kayvan <kayvan.goli@gmail.com>
2019-10-06 06:45:16 +0330

    Remove check dice box from incorrect place in player and raise thrown event for dice box

--- app/src/main/java/com/k1apps/backgammon/gamelogic/Player.kt
+++ app/src/main/java/com/k1apps/backgammon/gamelogic/Player.kt
@@ -2,6 +2,7 @@ package com.k1apps.backgammon.gamelogic
 
 import com.k1apps.backgammon.Constants.NORMAL_HOME_RANGE
 import com.k1apps.backgammon.Constants.REVERSE_HOME_RANGE
+import com.k1apps.backgammon.gamelogic.event.DiceBoxThrownEvent
 import com.k1apps.backgammon.gamelogic.event.DiceThrownEvent
 import org.greenrobot.eventbus.EventBus
 import java.util.ArrayList
@@ -22,27 +23,13 @@ class PlayerImpl(
 
 
     override fun roll() {
-        when {
-            dice != null -> {
-                dice!!.roll()
-                EventBus.getDefault().post(DiceThrownEvent(this))
-            }
-            diceBox != null -> {
-                diceBox!!.roll()
-                for (piece in pieceList) {
-                    val number1 = diceBox!!.dice1.number!!
-                    val number2 = diceBox!!.dice2.number!!
-                    piece.pieceAfterMove(number1)?.let {
-                        if (board.canMovePiece(homeCellIndexRange, it, number1)) {
-                            diceBox!!.canUseDiceWith(number1)
-                        }
-                    }
-                    piece.pieceAfterMove(number2)?.let {
-                        if (board.canMovePiece(homeCellIndexRange, it, number2)) {
-                            diceBox!!.canUseDiceWith(number2)
-                        }
-                    }
-                }
+        dice?.let {
+            it.roll()
+            EventBus.getDefault().post(DiceThrownEvent(this))
+        } ?: run {
+            diceBox?.let {
+                it.roll()
+                EventBus.getDefault().post(DiceBoxThrownEvent(this))
             }
         }
     }
--- app/src/main/java/com/k1apps/backgammon/gamelogic/event/DiceThrownEvent.kt
+++ app/src/main/java/com/k1apps/backgammon/gamelogic/event/DiceThrownEvent.kt
@@ -3,3 +3,5 @@ package com.k1apps.backgammon.gamelogic.event
 import com.k1apps.backgammon.gamelogic.Player
 
 data class DiceThrownEvent(val player: Player)
+
+data class DiceBoxThrownEvent(val player: Player)
