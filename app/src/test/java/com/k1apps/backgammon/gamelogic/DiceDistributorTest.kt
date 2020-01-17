2f1f5816b27b5f6fda80d8888439e7268381b3cf
kayvan <kayvan.goli@gmail.com>
2020-01-17 22:13:36 +0330

    Refactor DiceDistributorTest and improve DiceDistributor

--- app/src/test/java/com/k1apps/backgammon/gamelogic/DiceDistributorTest.kt
+++ app/src/test/java/com/k1apps/backgammon/gamelogic/DiceDistributorTest.kt
@@ -15,38 +15,40 @@ import org.mockito.junit.MockitoJUnitRunner
 class DiceDistributorTest {
 
     @Mock
-    lateinit var diceBox: DiceBox
+    private lateinit var dice1: Dice
 
     @Mock
-    lateinit var player1: Player
+    private lateinit var dice2: Dice
+
+    private lateinit var diceBox : DiceBox
 
     @Mock
-    lateinit var player2: Player
+    private lateinit var player1: Player
 
-    lateinit var diceDistributor : DiceDistributor
+    @Mock
+    private lateinit var player2: Player
+
+    private lateinit var diceDistributor: DiceDistributor
 
     @Before
     fun setup() {
+        diceBox = DiceBoxImpl(dice1, dice2)
         diceDistributor = DiceDistributorImpl(player1, player2, diceBox)
     }
-
     @Test
-    fun when_dice_distributor_started_then_each_player_must_have_dice() {
-        val dice1 = mock(Dice::class.java)
-        `when`(diceBox.dice1).thenReturn(dice1)
-        val dice2 = mock(Dice::class.java)
-        `when`(diceBox.dice2).thenReturn(dice2)
+    fun given_started_then_each_of_two_players_must_have_dice() {
         diceDistributor.start()
         verify(player1, times(1)).dice = dice1
         verify(player2, times(1)).dice = dice2
     }
 
     @Test
-    fun given_DiceThrownEvent_when_player1_dice_num_is_6_and_player2_is_2_then_set_dice_box_to_player1_and_retake_dices() {
-        `when`(diceBox.dice1.number).thenReturn(6)
-        `when`(diceBox.dice2.number).thenReturn(2)
+    fun given_player1_DiceThrownEvent_called_when_player1_has_dice_and_num_is_6_and_player2_has_dice_with_number_2_then_retake_dices_from_players_and_set_dice_box_to_player1() {
+        `when`(dice1.number).thenReturn(6)
+        `when`(dice2.number).thenReturn(2)
+        player1.dice = dice1
+        player2.dice = dice2
         diceDistributor.onEvent(DiceThrownEvent(player1))
-        diceDistributor.onEvent(DiceThrownEvent(player2))
         verify(player1, times(1)).diceBox = diceBox
         verify(player2, times(0)).diceBox = diceBox
         verify(player1, times(1)).retakeDice()
@@ -55,24 +57,19 @@ class DiceDistributorTest {
     }
 
     @Test
-    fun when_player1_dice_num_is_1_and_player2_is_4_then_set_dice_box_to_player2_and_retake_dice() {
-        diceDistributor.start()
-        `when`(diceBox.dice1.number).thenReturn(1)
-        `when`(diceBox.dice2.number).thenReturn(4)
-        diceDistributor.onEvent(DiceThrownEvent(player2))
+    fun given_player1_DiceThrownEvent_called_when_player1_has_dice_and_num_is_3_and_player2_does_not_yet_rolled_dice_then_player1_and_player2_must_have_dice() {
+        `when`(dice1.number).thenReturn(3)
+        `when`(dice2.number).thenReturn(null)
         diceDistributor.onEvent(DiceThrownEvent(player1))
-        verify(player1, times(0)).diceBox = diceBox
-        verify(player2, times(1)).diceBox = diceBox
-        verify(player1, times(1)).retakeDice()
-        verify(player2, times(1)).retakeDice()
+        verify(player1, times(0)).retakeDice()
+        verify(player2, times(0)).retakeDice()
     }
 
     @Test
-    fun when_player1_dice_num_is_equal_to_player2_then_retake_dices_and_restart_roll_dices() {
-        `when`(diceBox.dice1.number).thenReturn(2)
-        `when`(diceBox.dice2.number).thenReturn(2)
+    fun given_DiceThrown_occurred_when_player1_dice_num_is_equal_to_player2_then_retake_dices_and_restart_roll_dices() {
+        `when`(dice1.number).thenReturn(2)
+        `when`(dice2.number).thenReturn(2)
         diceDistributor.onEvent(DiceThrownEvent(player1))
-        diceDistributor.onEvent(DiceThrownEvent(player2))
         verify(player1, times(1)).dice = diceBox.dice1
         verify(player2, times(1)).dice = diceBox.dice2
         verify(player1, times(1)).retakeDice()
@@ -80,54 +77,41 @@ class DiceDistributorTest {
     }
 
     @Test
-    fun when_player1_has_dice_box_then_return_player1_on_whichPlayerHasDiceBox() {
-        player1.diceBox = diceBox
-        assertTrue(diceDistributor.whichPlayerHasDice()?.first == player1)
+    fun given_whichPlayerHasDiceBox_called_when_player1_has_dice_box_then_return_player1() {
+        `when`(player1.diceBox).thenReturn(diceBox)
+        assertTrue(diceDistributor.whichPlayerHasDice()?.first === player1)
     }
 
     @Test
-    fun when_player2_has_dice_box_then_return_player2_on_whichPlayerHasDiceBox() {
-        player2.diceBox = diceBox
-        assertTrue(diceDistributor.whichPlayerHasDice()?.first == player2)
+    fun given_whichPlayerHasDiceBox_called_when_player2_has_dice_box_then_return_player2() {
+        `when`(player2.diceBox).thenReturn(diceBox)
+        assertTrue(diceDistributor.whichPlayerHasDice()?.first === player2)
     }
 
     @Test
-    fun when_whichPlayerHasDice_called_and_both_player_has_dice_then_return_both_players() {
-        `when`(player1.dice).thenReturn(mock(Dice::class.java))
-        `when`(player2.dice).thenReturn(mock(Dice::class.java))
+    fun given_whichPlayerHasDice_called_when_both_player_has_dice_then_return_both_players() {
+        `when`(player1.dice).thenReturn(dice1)
+        `when`(player2.dice).thenReturn(dice2)
         val whichPlayerHasDice = diceDistributor.whichPlayerHasDice()
-        verify(player1, times(1)).dice
-        verify(player2, times(1)).dice
         assertTrue(whichPlayerHasDice!!.first == player1)
         assertTrue(whichPlayerHasDice.second == player2)
     }
 
     @Test
-    fun when_dice_box_thrown_then_invoke_player_update_dice() {
-        val player = mock(Player::class.java)
-        `when`(diceBox.isAtLeastOneDiceEnable()).thenReturn(true)
-        `when`(player.diceBox).thenReturn(diceBox)
-        val diceBoxThrownEvent = DiceBoxThrownEvent(player)
+    fun given_DiceBoxThrownEvent_called_by_player1_when_player1_has_diceBox_then_player1_updateDicesStateInDiceBox_should_be_called() {
+        `when`(player1.diceBox).thenReturn(diceBox)
+        val diceBoxThrownEvent = DiceBoxThrownEvent(player1)
         diceDistributor.onEvent(diceBoxThrownEvent)
-        verify(player, times(1)).updateDicesStateInDiceBox()
+        verify(player1, times(1)).updateDicesStateInDiceBox()
     }
 
     @Test
-    fun when_dice_box_thrown_and_after_player_updated_dice_box_to_disable_then_retake_dice_box_from_player_set_dice_box_to_opponent() {
-        val diceBoxThrownEvent = DiceBoxThrownEvent(player1)
-        `when`(diceBox.dice1.number).thenReturn(4)
-        `when`(diceBox.dice2.number).thenReturn(4)
-        player1.diceBox = diceBox
+    fun given_DiceBoxThrownEvent_called_by_player1_when_player1_has_diceBox_and_diceBox_does_not_have_enable_dice_then_retake_diceBox_from_player1_and_set_it_to_player2() {
+        `when`(player1.diceBox).thenReturn(diceBox)
         `when`(diceBox.isAtLeastOneDiceEnable()).thenReturn(false)
+        val diceBoxThrownEvent = DiceBoxThrownEvent(player1)
         diceDistributor.onEvent(diceBoxThrownEvent)
         verify(player1, times(1)).retakeDiceBox()
         verify(player2, times(1)).diceBox = diceBox
     }
-
-//    @Test
-//    fun given_whichPlayerHasDiceBox_when_player1_has_diceBox_then_return_player1() {
-//        player1.diceBox = diceBox
-//        assertTrue(diceDistributor.whichPlayerHasDiceBox() === player1)
-//    }
-
 }
